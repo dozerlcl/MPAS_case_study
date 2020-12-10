db2c1de3c3c6dff752fe9aa9116368eda06eec62
dozerlcl <yiyuan_cheng@foxmail.com>
2020-12-10 19:48:46 +0800

    add space taylor in every month

--- case_template/92-25km_vr_case_postprocess/tricks/transfer.sh
+++ case_template/92-25km_vr_case_postprocess/tricks/transfer.sh
@@ -1,27 +1,28 @@
-selectyear=2003
-modtype=RCM
-# modtype=VR
-part=postprocess
+selectyear=2006
+# modtype=RCM
+modtype=VR
 # part=
+# part=postprocess
+part=out
 
 # .19
-# here=/raid52/yycheng/MPAS/92-25km_${modtype}_${selectyear}/
+here=/raid52/yycheng/MPAS/92-25km_${modtype}_${selectyear}/
 # .11
 # here=/raid51/yycheng/MPAS/92-25km_${modtype}_${selectyear}/
 #.132
-here=/data2/yycheng/MPAS/92-25km_${modtype}_${selectyear}/${part}
+# here=/data2/yycheng/MPAS/92-25km_${modtype}_${selectyear}/${part}
 
 # .19
-there=/raid52/yycheng/MPAS/92-25km_${modtype}_${selectyear}/${part}
+# there=/raid52/yycheng/MPAS/92-25km_${modtype}_${selectyear}/${part}
 # .11
-# there=/raid51/yycheng/MPAS/92-25km_${modtype}_${selectyear}/
+there=/raid51/yycheng/MPAS/92-25km_${modtype}_${selectyear}/
 #.132
 # there=/data2/yycheng/MPAS/92-25km_${modtype}_${selectyear}/
 
 # .19
-scp -r ${here} yycheng@114.212.49.19:${there}
+# scp -r ${here} yycheng@114.212.49.19:${there}
 # .11
-# scp -r ${here} yycheng@114.212.49.11:${there}
+scp -r ${here} yycheng@114.212.49.11:${there}
 # .132
 # scp -r ${here} yycheng@114.212.49.132:${there}
 
