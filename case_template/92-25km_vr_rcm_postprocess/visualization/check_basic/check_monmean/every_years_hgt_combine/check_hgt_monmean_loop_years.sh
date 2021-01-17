c4797b5618c91ab8b5ceb789655c43798f0ce6d8
dozerlcl <yiyuan_cheng@foxmail.com>
2021-01-18 06:03:05 +0800

    month mean plot

--- case_template/92-25km_vr_rcm_postprocess/visualization/check_basic/check_monmean/every_years_hgt_combine/check_hgt_monmean_loop_years.sh
+++ case_template/92-25km_vr_rcm_postprocess/visualization/check_basic/check_monmean/every_years_hgt_combine/check_hgt_monmean_loop_years.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 start_time=`date +%s`              #定义脚本运行的开始时间
 years=$(seq 2003 1 2015 )
+export sellevel='200'
 for year_select in ${years[@]}
-
 do
     {
-        mkdir -p ./output_pic/RCM/${year_select}
+        mkdir -p ./output_pic/years/${year_select}
         export selyear=${year_select}
-        ncl -Q ./check_hgt_monmean.ncl
+        ncl -Q ./check_hgt_monmean_combine_years.ncl
     }&
 
 done
