819f5290eb49a9a92723e33333c25e3298128b12
dozerlcl <yiyuan_cheng@foxmail.com>
2021-04-08 04:30:56 +0800

    citied annual cycle and interannual

---
+++ 92-25km_vr_rcm_postprocess/visualization/check_obs/check_cn051/t2m/space/interannual_preprocess.sh
@@ -0,0 +1,27 @@
+# 2021.04.07
+#   计算年际的相关的cdo预处理部分,拆分出4-5月以及6-8月的数据,配合 corr_interannual 执行
+#   IN:
+input=/raid52/yycheng/MPAS/REFERENCE/TEMP_DATA/mask_t2m/mask_res
+#   OUT:
+# output=/raid52/yycheng/MPAS/REFERENCE/TEMP_DATA/mask_t2m/mask_res_dayofyear
+output=/raid52/yycheng/MPAS/REFERENCE/TEMP_DATA/mask_t2m/mask_res_interannual
+
+
+echo "------------preprocess dir:"$target_path"-----------"
+
+for infile in `ls ${input}/*.nc`;
+
+do
+#----
+# read -u3                           #代表从管道中读取一个令牌
+#----
+{
+    echo "${infile} exist!"
+    outfile="`basename ${infile}`"
+    cdo -yearmean -selmonth,4/5 $infile ${output}/am_${outfile}
+    cdo -yearmean -selmonth,6/8 $infile ${output}/jja_${outfile}
+    # cdo ydaymean ${infile} ${output}/ydaymean_${outfile}    
+
+}&
+
+done
