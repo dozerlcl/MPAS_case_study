c1285f6f081a188fa329bef4fa67c0ed5a553bcc
dozerlcl <yiyuan_cheng@foxmail.com>
2021-08-08 06:01:55 +0800

    merge cn051 and corph obs data

--- 92-25km_vr_rcm_postprocess/visualization/check_obs/pre_process/precip_count_loop_year.sh
+++ 92-25km_vr_rcm_postprocess/visualization/check_obs/pre_process/precip_count_loop_year.sh
@@ -2,7 +2,7 @@
 # loop precip_count.ncl脚本计算各个年份的降水
 # 2021.05.10 修改为从diag中获取降水数据，运行 precip_count_diag.ncl
 start_time=`date +%s`              #定义脚本运行的开始时间
-years=$(seq 1998 1 2002)
+years=$(seq 1998 1 2017)
 for year_select in ${years[@]}
 
 do
